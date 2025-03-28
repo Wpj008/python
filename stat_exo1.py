5204843072b6e63a7124eac4d628a0d4aee3c521
WPJ008 <wawitephilemon@gmail.com>
2025-03-28 21:56:41 +0100

    exo 1 stat

---
+++ stat_exo1.py
@@ -0,0 +1,9 @@
+#somme de 2 nombres
+
+nombre1 = int(input("saisir le premier nombre : "))
+nombre2 = int(input("Saisir le deuxième nombre : "))
+
+Resultat = nombre1 + nombre2
+
+
+print(f"{nombre1} + {nombre2} = {Resultat}")
