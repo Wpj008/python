fa02231d1f68a7808c1c7d90744c9cf74d454a16
WPJ008 <wawitephilemon@gmail.com>
2025-05-11 13:06:26 +0200

    update List

--- List.py
+++ List.py
@@ -1,11 +1,33 @@
-def saisir():
-    saisies = input("Entrez les valeurs (tapez 'fin' pour terminer) : ")
+def menu():
+    reponse = input("voulez vous saisir une autre liste ? OUI / NON : ")
 
-    valeurs = [valeur.strip() for valeur in saisies.split(',')]
-    if 'fin' in valeurs:
-        valeurs = valeurs[:valeurs.index('fin')]
-        return valeurs
-    
-valu = saisir()
+    if reponse == 'oui' or reponse == 'OUI':
 
-print("Les valeurs saisies sont : ", valu)
+       saisie = input("Entrez les elements de la liste separés par des virgules : ")
+
+       La = saisie.split(",")
+       unionList(La)
+
+    else: 
+
+        return 0 
+
+
+def unionList(L1):
+
+    L = L1 
+# la structure list(set(nom de la liste)) permet de supprimer les doublons
+    L4 = list(set(L))
+#la fonction sorted permet que la liste soit trier en ordre
+    LF = sorted(L4)
+
+    print(f'Liste après tri :{LF}')
+
+    menu()
+
+saisie = input("Entrez les elements de la liste separés par des virgules : ")
+
+Lo = saisie.split(",")
+print(Lo)
+
+unionList(Lo)
