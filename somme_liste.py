5855cca939e45cfaff55fd2a7ff441fe47f741f3
WPJ008 <wawitephilemon@gmail.com>
2025-10-20 12:18:42 +0200

    somme liste

---
+++ somme_liste.py
@@ -0,0 +1,13 @@
+def sommeList(L):
+ 
+    somme = 0
+    for i in L:
+
+        somme = somme + i
+
+  
+    print(f"La somme de tous les élements de la liste est de {somme}")
+
+L = [1, 2, 4, 5]
+
+sommeList(L)
